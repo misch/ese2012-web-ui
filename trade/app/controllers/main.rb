54e552130d3872fbc4a22c495cae2f09b06ee64c
Michele Wyss <michele_wyss@students.unibe.ch>
2012-09-30 11:30:03 +0200

    set the right views-directory in main.rb - now it seems to do something, finally.

--- trade/app/controllers/main.rb
+++ trade/app/controllers/main.rb
@@ -1,14 +1,23 @@
-require 'rubygems'
-require 'sinatra'
-require 'haml'
-
-module Controllers
-
-  class Main < Sinatra::Application
-    get '/' do
-      haml :hello
-    end
-  end
+def relative(path)
+  File.join(File.expand_path(File.dirname(__FILE__)), path)
 end
+Views = relative('../views')
+require 'rubygems'
+require 'require_relative'
+require 'sinatra/base'
+
+#require_relative('controllers/init.rb')
+#require_relative('../fixture.rb')
+
+class Main < Sinatra::Base
+  enable :sessions
+  set :views, Views
+#  use Controllers::Authentication
+#  use Controllers::TextController
+
+  get("/") { haml :hello }
+end
+
+Main.run!
 
 
